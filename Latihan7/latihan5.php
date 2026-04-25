208ce1580d98bd516eabe5bc0b74f1e1fb9c5a03
Daffa katianda <116802129+dkatianda44@users.noreply.github.com>
2026-04-25 10:47:51 +0700

    Update latihan5.php

--- Latihan7/latihan5.php
+++ Latihan7/latihan5.php
@@ -6,5 +6,4 @@ echo "Jenis Buku & Hiburan :<br />";
 echo "Cerpen : $Majalah<br />";
 echo "Cerita Bergambar : $Komik<br />";
 echo "Bioskop : $Film";
-//Project 7.5 By ASEP SURYA AGUSTIN - 221011450392
 ?>
